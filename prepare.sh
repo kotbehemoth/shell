f95f21852902d434b274e90900c1c2698d17a8b6
kotbehemoth <kotbehemoth@tlen.pl>
2017-02-09 11:24:45 +0800

    Fixed redirections in g/gg, renamed variables

--- prepare.sh
+++ prepare.sh
@@ -16,26 +16,27 @@ check_link()  # params: final_link_path absolute_path
     src=$2
     dirname=`dirname $link`
     if ! [ -d $dirname ]; then
-        echo "Creatin directoy $dirname ..."
+        echo "Creating directory $dirname ..."
         mkdir -p $dirname
     fi
     if [ -e $link ]; then
         if [ -h $link ] && [ "`readlink $link`" == "$src" ]; then
             echo "$link already installed to point to $src, skipping ..."
-            return
+            return 0
         fi
-        echo -n "File $link already exist, backup and proceed? [y/N] "
+        echo -n "File $link already exist, backup original file and proceed with installation? [y/N] "
         read choice
         if [ "$choice" == "y" ]; then
-            echo "Backing up to $link.bak ..."
+            echo "Ranaming to $link.bak ..."
             mv $link $link.bak
         else
             echo "Skipping ..."
-            return
+            return 1
         fi
     fi
     echo "Creating link $link -> $src ..."
     ln -s $src $link
+    return $?
 }
 
 # screen
@@ -43,6 +44,7 @@ check_link $HOME/.screenrc $dir/screenrc
 
 # vim
 check_link $HOME/.vimrc $dir/vimrc
+VIMRC=$?
 for plugin in `(cd vim; find -type f -name *.vim -printf '%P\n')`; do
     check_link $HOME/.vim/$plugin $dir/vim/$plugin
 done
@@ -54,25 +56,27 @@ cat << EOF
 - Source $dir/shrc in your start script
 
 Configuration global variables:
-TABSPACES=X             - use X spaces instead of <TAB> character in vim
-PRETTYPROMPT=[yes|no]   - modify shell prompt
-SHOWWINDOWNAME=[yes|no] - set screen's window name in prompt
-SHOWHOSTNAME=[yes|no]   - print hostname in window name
-SHOWBRANCHANME=[yes|no] - print branch name in prompt
+TAB_SPACES=X                    - use X spaces instead of <TAB> character in vim
+PROMPT_ENABLED=[yes|no]         - modify shell prompt
+PROMPT_SCREEN=[yes|no]          - set screen's window name in prompt
+PROMPT_WINDOW_HOSTNAME=[yes|no] - print hostname in window name
 
 E.g.: .bashrc:
-export TABSPACES=4
-export PRETTYPROMPT=yes
-export SHOWWINDOWNAME=yes
-export SHOWHOSTNAME=no
-export SHOWBRANCHNAME=yes
+export TAB_SPACES=4
+export PROMPT_ENABLED=yes
+export PROMPT_SCREEN=yes
+export PROMPT_WINDOW_HOSTNAME=no
 export PATH="\${PATH}:$dir/bin"
 source $dir/shrc
+EOF
+
+if [ $VIMRC -ne 0 ] ; then
+cat << EOF
 
 # VIM CONFIGURATION
-If installing .vimrc was skipped you can include the vimrc directly in your start script
+You can include the vimrc directly in your start script
 E.g.: .vimrc
 source $dir/vimrc
-
 EOF
+fi
 
